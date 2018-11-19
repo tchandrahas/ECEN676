3865ffcef9109e65be74b6d0f21e414c89b72d02
Chandrahas Tirumalasetty <chandrahas@Chandrahass-MacBook-Pro.local>
2018-11-19 02:10:26 -0600

    Removed a bug in XBar.py

--- gem5-stable_2015_09_03/src/mem/XBar.py
+++ gem5-stable_2015_09_03/src/mem/XBar.py
@@ -134,17 +134,6 @@ class L3XBar(CoherentXBar):
     forward_latency = 0
     response_latency = 1
     snoop_response_latency = 1
-
-    # Use a snoop-filter by default, and set the latency to zero as
-    # the lookup is assumed to overlap with the frontend latency of
-    # the crossbar
-    snoop_filter = SnoopFilter(lookup_latency = 0)
-
-    # This specialisation of the coherent crossbar is to be considered
-    # the point of unification, it connects the dcache and the icache
-    # to the first level of unified cache.
-    point_of_unification = True
-    
 # One of the key coherent crossbar instances is the system
 # interconnect, tying together the CPU clusters, GPUs, and any I/O
 # coherent masters, and DRAM controllers.
